f57cea1fb95b952da95859cbd9243050c6fdd164
muhammad ilham <mi1797128@gmail.com>
2024-02-23 17:47:02 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,7 @@
+// IAM Coding Challenge 2024
+// day 1/366
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
 const grow =(x)=> x.reduce((acc,curr) => acc* curr);//fucntion expresion
 
 console.log(grow([1, 2, 3, 4]));
